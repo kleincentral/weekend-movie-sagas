96b14a9e04a9f1fb70ea13b3c8a7d1dd97b72b0c
Mark Klein <kleincentral@gmail.com>
2023-12-08 15:28:43 -0600

    Details pages now links to the movies saga page and passes the first test

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Details() {
   return (
-    <div>
+    <div data-testid="movieDetails">
       <Link to="/">
-        <p>Home</p>
+        <button data-testid="toList">back to movie list</button>
       </Link>
     </div>
   );
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieList.css";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 function MovieList() {
-
   const dispatch = useDispatch();
-  const movies = useSelector(store => store.movies);
+  const movies = useSelector((store) => store.movies);
 
   useEffect(() => {
-    dispatch({ type: 'FETCH_MOVIES' });
+    dispatch({ type: "FETCH_MOVIES" });
   }, []);
 
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
-            <div data-testid='movieItem' key={movie.id}>
+            <div data-testid="movieItem" key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <Link to="/details">
+                <img
+                  data-testid="toDetails"
+                  src={movie.poster}
+                  alt={movie.title}
+                />
+              </Link>
             </div>
           );
         })}
       </section>
     </main>
-
   );
 }
-
 
 export default MovieList;
