69359c8dc1bf423ca31584b8e9ae31fba5dd6598
Mark Klein <kleincentral@gmail.com>
2023-12-08 16:01:43 -0600

    Added some router stuff for genra rendering so we can get the genera for our client

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector } from "react-redux";
 
 export default function Details() {
+  const ourMovie = useSelector((store) => store.selectedMovie);
+
   return (
     <div data-testid="movieDetails">
       <Link to="/">
         <button data-testid="toList">back to movie list</button>
       </Link>
+      <br></br>
+      <h3>{ourMovie.title}</h3>
+      <img src={ourMovie.poster} alt={ourMovie.title} />
+      <p>{ourMovie.description}</p>
     </div>
   );
 }
