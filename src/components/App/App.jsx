12b424446187fdfc5f1153c59f2144db401524dc
Mark Klein <kleincentral@gmail.com>
2023-12-08 15:19:29 -0600

    Details page exists and can be used to navigate home

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,25 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList';
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import MovieList from "../MovieList/MovieList";
+import Details from "../Details/Details";
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
+
+        <Route path="/details" exact>
+          <Details />
+        </Route>
 
         {/* Add Movie page */}
-        
       </Router>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+export default function Details() {
+  return (
+    <div>
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+    </div>
+  );
+}
